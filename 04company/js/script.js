afc0c784b6aa358d5f25b7305439cf7a2fa85d6b
limayoung123 <81205781+limayoung123@users.noreply.github.com>
2021-05-03 18:17:15 +0900

    0503
    삼첩분식 새소식까지 만들었음
    모바일 버전 햄버줄 클릭시 메뉴 나오기,
    모바일 버전시 슬라이더 이미지 줄이는법

--- 04company/js/script.js
+++ 04company/js/script.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
   $(".slider").bxSlider({
-    //auto:true
+    auto:true
   });
 });
+
+// $(".toggle").click(function(){
+// const toggle1 = $(".toggle").attr("class");
+// if(toggle1 == "toggle"){
+// $(".toggle").addClass("change");
+//  $("#main_menu").stop().animate({top:0});
+//  }else{
+// $(".toggle").removeClass("change");
+//  $("#main_menu").stop().animate({top:-100%});
+// }
+// });
