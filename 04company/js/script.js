386580ad8f78df8b7cf14b5f3bcb7b2c99fae8c4
limayoung123 <81205781+limayoung123@users.noreply.github.com>
2021-05-11 18:22:20 +0900

    0511
    해야할부분

--- 04company/js/script.js
+++ 04company/js/script.js
@@ -2,15 +2,17 @@ $(document).ready(function(){
   $(".slider").bxSlider({
     auto:true
   });
-});
 
-// $(".toggle").click(function(){
-// const toggle1 = $(".toggle").attr("class");
-// if(toggle1 == "toggle"){
-// $(".toggle").addClass("change");
-//  $("#main_menu").stop().animate({top:0});
-//  }else{
-// $(".toggle").removeClass("change");
-//  $("#main_menu").stop().animate({top:-100%});
-// }
-// });
+
+$(".toggle").click(function(){
+const tog = $(".toggle").attr("class");
+if(tog == "toggle"){
+$(".toggle").addClass("change");
+$("#main_menu").stop().animate({top:0});
+ }else{
+$(".toggle").removeClass("change");
+$("#main_menu").stop().animate({top:"-100%"});
+}
+});
+});
+ // 토클메뉴 클릭시 바뀌게 하는것
