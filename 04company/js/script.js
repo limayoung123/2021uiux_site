577a47ffe35e009293832dc597c73296261a7d91
limayoung123 <81205781+limayoung123@users.noreply.github.com>
2021-05-14 16:19:20 +0900

    pc
    pc서브메뉴 완성

--- 04company/js/script.js
+++ 04company/js/script.js
@@ -17,14 +17,29 @@ $("#main_menu").stop().animate({top:"-100%"});
 $(window).resize(function(){
       $("#main_menu").removeAttr("style");
       $(".toggleMenu").removeClass("change");
+       winWidth = $(window).innerWidth()
+       menuOver();
 });
 
-// toggle2영역하기
+//menuOVer라는 함수를 만들어준다
+//innerWidth로 길이값을 if문으로 해주고 widWidth라는 변수로 지정해서 적용
+//resize는 화면의 크기를 바꿔도 계속 적용되게하는것!
 
+let winWidth = $(window).innerWidth();
+menuOver();
+
+
+function menuOver(){
 $("#main_menu > ul > li").mouseover(function(){
-  $(this).find(".sub1").stop().slideDown(500);
+  if(winWidth < 700){ return false }else{
+  $(this).find(".sub1").stop().slideDown(500);}
 });
 $("#main_menu > ul > li").mouseout(function(){
-  $(this).find(".sub1").stop().slideUp(500);
+  $(this).find(".sub1" ).stop().slideUp(500);
 });
+}
+
+
+
+
 });
